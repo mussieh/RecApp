60a6c7ea3e9b2c3c56fc028a07a5ef23b98a6225
Mussie Habtemichael <mussie.habtemichael2@gmail.com>
2018-04-12 00:03:16 -0500

    Update DeveloperKey.java

--- app/src/main/java/com/mussieh/recapp/data/DeveloperKey.java
+++ app/src/main/java/com/mussieh/recapp/data/DeveloperKey.java
@@ -3,9 +3,9 @@ package com.mussieh.recapp.data;
 /**
  * Created by Mussie on 3/12/2018.
  * Class to hold the user's developer key
- * Note: in the final application, never store a developer key in class files, but fetch the key
+ * Note: In the final application, never store a developer key in class files, but fetch the key
  * from an online source during runtime.
  */
 public class DeveloperKey {
-    public static final String DEVELOPER_KEY = "AIzaSyCQJMr4Q8VJEKSgT-ugHgEWJ-vj96dVCiQ";
+    public static final String DEVELOPER_KEY = "YOUR_DEVELOPER_KEY";
 }
